2ccdfe474dce954cb9fd7c04f3a9aef3c4c831aa
levibland <49114610+levibland@users.noreply.github.com>
2020-09-01 10:47:44 +1200

    Create bitwiseoperators.py
    Show an example use of bitwise operators.

---
+++ math/bitwiseoperators.py
@@ -0,0 +1,57 @@
+# bitwise AND operator examples
+
+# print 0
+print(0 & 0)
+
+# print 0
+print(0 & 1)
+
+# print 0
+print(1 & 0)
+
+# print 1
+print(1 & 1)
+
+
+# bitwise OR operator examples
+
+#print 0
+print(0 | 0)
+
+#print 1
+print(0 | 1)
+
+#print 1
+print(1 | 0)
+
+#print 1
+print(1 | 1)
+
+# bitwise XOR operator examples
+
+# print 0
+print(0 ^ 0)
+
+# print 1
+print(0 ^ 1)
+
+#print 1
+print(1 ^ 0)
+
+#print 0
+print(1 ^ 1)
+
+
+# bitwise compliment operator
+
+# print 0
+print(~1)
+
+
+# bitwise shift
+
+# print 4 // left shift
+print(1 << 2)
+
+# print 1 // right shift
+print(3 >> 1)
