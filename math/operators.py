ed1f2e0e8449a66120526fd5adf7b164396e0eb0
levibland <49114610+levibland@users.noreply.github.com>
2020-09-01 10:28:52 +1200

    Add comments to operators.py
    Add comment to tell stupid readers what each operator is.

--- math/operators.py
+++ math/operators.py
@@ -1,10 +1,25 @@
 # math operators
 
+# addition operation
 print(5 + 3)
+
+# subtraction operation
 print(4 - 1)
+
+# division operation
 print(9 / 3)
+
+# multiplication operation
 print(4 * 9)
+
+# exponential operation
 print(5 ** 4)
+
+# modulo operation
 print(6 % 2)
+
+# modulo operation that returns a remainder
 print(4 % 3)
+
+# floor division operation
 print(9 // 2)
