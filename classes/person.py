c185b4ae0f69588bce27b8f2c84772d511f7fb23
levibland <49114610+levibland@users.noreply.github.com>
2020-09-03 09:40:08 +1200

    Create person.py

---
+++ classes/person.py
@@ -0,0 +1,21 @@
+# declare a new class called person
+class Person:
+    def __init__(self, age, fname, sname, job):
+          self.age = age
+          self.fname = fname
+          self.sname = sname
+          self.job = job
+    
+    def greet(greeting):
+          # use fstring to print greeting
+          print(f"{greeting}\n")
+ 
+
+# declare a new instance of the Person() class called tyrone
+tyrone = Person(40, "Tyrone", "Smith", "carpenter")
+
+# print tyrones attributes
+print(tyrone.age)
+print(tyrone.fname)
+print(tyrone.sname)
+print(tyrone.job)
