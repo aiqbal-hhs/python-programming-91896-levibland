ac96fa6b2eca698c66c2b016d1db51659bd612b9
levibland <49114610+levibland@users.noreply.github.com>
2020-09-03 10:02:50 +1200

    Create ascii.py

---
+++ char/ascii.py
@@ -0,0 +1,7 @@
+# script to get ascii value for a character
+
+# user inputs a char
+input_char = input("Input a char: ")
+
+# print the ascii value of input_char.
+print(f"The ascii value of {input_char} is {ord(input_char)}")
