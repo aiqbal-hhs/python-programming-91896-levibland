44459f10e495e0144a0f06e823de91c22407428f
levibland <49114610+levibland@users.noreply.github.com>
2020-09-03 12:37:52 +1200

    Update fibonacci.py

--- math/fibonacci.py
+++ math/fibonacci.py
@@ -1,1 +1,10 @@
+# a script that prints fibonacci numbers up to the limit set by the limit parameter
+def fibonacci(limit):
+  a, b = 0, 1
+  while a < limit:
+    yield a
+    a, b = b, a + b
 
+limit = int(input("Choose limit: "))
+for i in fibonacci(limit):
+  print(i)
