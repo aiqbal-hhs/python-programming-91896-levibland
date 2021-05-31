fb111a79398c9eeee50c87380fe5a65d873049cc
levibland <49114610+levibland@users.noreply.github.com>
2021-06-01 10:50:12 +1200

    Add turtle code

---
+++ turtle/turtleanimation.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle1 = turtle.Turtle()
+
+turtle1.forward(150)
+turtle1.right(90)
+turtle1.forward(150)
+turtle1.right(90)
+turtle1.forward(150)
+turtle1.right(90)
+turtle1.forward(150)
+turtle1.right(90)
+turtle1.forward(150)
