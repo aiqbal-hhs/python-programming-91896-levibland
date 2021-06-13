854d14e4201d533a3f63e9d05e3362346e32670f
levibland <49114610+levibland@users.noreply.github.com>
2021-06-14 09:49:38 +1200

    asj,bsddebgj,nsdf
    WEFRWEGEHERGHERGH

---
+++ microbit/rock_paper_scissors.py
@@ -0,0 +1,30 @@
+hand = 0
+
+def on_gesture_shake():
+    global hand
+    hand = randint(0, 2)
+    if hand == 0:
+        basic.show_leds("""
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            """)
+    elif hand == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif hand == 2:
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
