df42144eb90cb36c050840586688c0fc27dcd5d3
levibland <49114610+levibland@users.noreply.github.com>
2021-06-27 03:02:18 -0700

    Create names.py

---
+++ game/names.py
@@ -0,0 +1,11 @@
+# names file
+
+first_names = ['John', 'Jhon', 'Jonh', 'Paul', 'Pual', 'Jose', 'Joeseph', 'Matt', 'Mat', 'Matty E.', 'Ryoon', 'Ray', 'Bronson', 'Branson', 'Richard', 'Chamath', 'Aryan', 'Robert', 'Bob', 'Manu', 'Tupou', 'Matthew', 'Maphew', 'Mike', 'Michael', 'Kimbal', 'Marcus', 'Julian', 'Julius', 'Hunter', 'Darcy', 'James', 'Tangata', 'Tamaki', 'Tamariki', 'Ariki', 'Mana', 'Maan', 'Manulangi', 'Masanate', 'Botchulismo', 'Crotch face mcghee', 'Ryan', 'Little King', 'Henry', 'Marley', 'Max', 'Mac', 'Steve', 'Stephen', 'Leo', 'Martin', 'Wesley', 'Chris', 'Clive', 'Thomas', 'Thom', 'Tom', 'Tohm', 'Joe', 'Tim', 'Timothy', 'Zhang', 'Zhing', 'Ping', 'Ling', 'Xing', 'Xang', 'Ding', 'Dong', 'Dang', 'Xin', 'Xi', 'Ng', 'Andrew', 'Andy', 'Lxii', 'Theodore', 'Theo', 'Milo', 'Glen', 'Glenn', 'Manne', 'Asif', 'Jo', 'Jesus', 'Jamal', 'Tyrone', 'Terry', 'Terrence']
+
+last_names = ['Masse', 'Kaufusi', 'Zhing-ping-ling-ping-fing-ding-ling', 'Lee', 'Te Tapu', 'Te Tangata', 'Te Tamaki', 'Te Iwi', 'Te China', 'Te Tapu Te Tapu Te Wairoa', 'Te Wairoa', 'Cena', 'Hogan', 'Autismo', 'Syndrome', 'Smith', 'McDonald', 'McAfee', 'McAfee, Mercy on his soul. May he RIP.', '3kg of DMT', 'Davis', 'Andrew Davis', 'A. Davis', 'E. Lee', 'Ardern', 'Christ', 'Gale', 'Sherring', 'Emmerson', 'Emmersonb', 'Emmersonson', 'Emmersonsonson', 'Iqbal', 'Abadi', 'Cooper', 'Cobbler', 'Shoemaker', 'Lard', 'The Bruce', 'Windsor']
+
+gang_names = ['Obama']
+
+street_names = ['John', 'Infinite', 'Queesn', 'Quay', '55th', 'Te Fangilanga', 'Vine', 'Great Northwest']
+
+street_suffixes = ['St', 'Rd', 'Wrf', 'Tce', 'Ave', 'Blv', 'Close', 'Loop', 'Way']
