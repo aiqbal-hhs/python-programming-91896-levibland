8ca04a99aeeba137696140292b4756a65126cc0a
levibland <49114610+levibland@users.noreply.github.com>
2021-06-28 13:23:55 +1200

    Create dictionary.py

---
+++ types/dictionary.py
@@ -0,0 +1,15 @@
+# dictionary
+
+# dictionaries are data structure that store key-value pairs
+
+dict = {"Jhon": "Cena", 8: 4}
+
+for item in list(dict):
+  # access values in the dictionary using a key
+  print(dict[item])
+
+# add items by creating a new key
+
+dict[6] = 6
+
+print(dict[6])
