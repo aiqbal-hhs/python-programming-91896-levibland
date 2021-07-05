74fff3fe4cc4e4251c72cf3237f5748a1b8fd36f
levibland <49114610+levibland@users.noreply.github.com>
2021-07-05 12:26:22 -0700

    Create main.py

---
+++ ordering_system/main.py
@@ -0,0 +1,14 @@
+###  IMPORTS  ###
+
+import pickle
+from typing import NoReturn
+
+# @notice main function
+def main() -> int:
+    print("Hello world!")
+
+    # exit with code 0, indicating success
+    return 0
+
+if __name__ == "__main__":
+    main()
